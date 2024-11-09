03f41f3b0813ff9dd482f1d48d088cee6533f8be
dem4ply <dem4ply@gmail.com>
2024-11-09 00:48:18 -0600

    se agregaron dependencias

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = [ 'chibi>=0.14.0', 'chibi_atlas>=1.1.0' ]
 
 setup(
     author="dem4ply",
